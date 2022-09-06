9d264481ff22adc67ac90c86d7f48a65ed624221
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-09-05 20:39:13 -0400

    Create QrInfo class

--- qr_app/models.py
+++ qr_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+class QrInfo(models.Model):
+    info = models.CharField(max_length= 200)
+    date = models.DateField(default=now)
+    image = models.ImageField()
