9327ddefd9b677ac5dc1e276015260bd0d5b9dd8
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-09-05 20:40:22 -0400

    Add url for qr_service

--- qrcode_generator/urls.py
+++ qrcode_generator/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('django.contrib.auth.urls')),
+    path('users', include('django.contrib.auth.urls')),
     path('home/', include('users.urls')),
+    path('qr_service/', include('qr_app.urls'))
 ]
