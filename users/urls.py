223a85d9f07236f4d66f4dadcc5bb0061e599cc0
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-09-05 20:42:56 -0400

    Add urls for login and password change

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = "home"),
+    path('', views.home_page, name = "home"),
     path("signup/", views.SignUp.as_view(), name="signup"),
-    path("login/", views.Login.as_view(), name="login")
+    path("login/", views.Login.as_view(), name="login"),
+    path("password_change", views.PasswordChange.as_view(), name="password_change")
 ]
