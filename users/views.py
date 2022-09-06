3e9128b0ee5f7172308876bb001ae504d80f0afe
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-09-05 20:43:21 -0400

    Create login, and signup views

--- users/views.py
+++ users/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from django.views.generic.edit import CreateView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 
 # Create your views here.
-def home(request):
-    return render(request, "users/home.html")
+def home_page(request):
+	return render (request,"users/home.html")
 
 class SignUp(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("home")
     template_name = "users/registration/signup.html"
 
 class Login(LoginView):
     form_class = AuthenticationForm
     template_name = "users/registration/login.html"
+
+class PasswordChange(PasswordChangeView):
+    form_class = PasswordChangeForm
+    template_name = "users/registration/password_change_form.html"
